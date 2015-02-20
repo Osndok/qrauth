f1a97726fc77a51cf68dc467d5b1b28a8e5053b4
Robert Hailey <rhailey@allogy.com>
2015-02-20 17:51:21 -0600

    f: RSAHelper handles most common formats and simplies /auth/dispatch logic

---
+++ java/qrauth-server/src/main/java/com/allogy/qrauth/server/helpers/RSAHelper.java
@@ -0,0 +1,216 @@
+package com.allogy.qrauth.server.helpers;
+
+import org.apache.commons.codec.binary.Base64;
+
+import java.io.*;
+
+/**
+ * In an attempt to support multiple common formats, this utility will use shell commands
+ * to convert between various public key formats.
+ */
+public
+class RSAHelper implements Closeable
+{
+	private static final
+	File SSH_KEYGEN = new File(System.getProperty("SSH_KEYGEN", "/usr/bin/ssh-keygen"));
+
+	private static final
+	File PUBKEY2SSH = new File(System.getProperty("PUBKEY2SSH", "/usr/bin/qrauth-pubkey2ssh"));
+
+	/**
+	 * @url http://rt.openssl.org/Ticket/Display.html?user=guest&pass=guest&id=2618
+	 */
+	private static final
+	boolean OPENSSL_BUG_2618_IS_STILL_WIDESPREAD = true;
+
+	public
+	RSAHelper(String pubKey) throws IOException
+	{
+		if (pubKey.contains("ssh-rsa"))
+		{
+			sshFormat = pubKey;
+		}
+		else
+		if (pubKey.contains("BEGIN SSH2 PUBLIC KEY"))
+		{
+			final
+			File file = File.createTempFile("qrauth-rsa-", ".ssh2");
+
+			try
+			{
+				writeBytesToFile(pubKey.getBytes(), file);
+				sshFormat = Exec.toString(SSH_KEYGEN.toString(), "-i", "-f", file.toString());
+			}
+			finally
+			{
+				file.delete();
+			}
+		}
+		else
+		if (pubKey.contains("BEGIN RSA PUBLIC KEY"))
+		{
+			final
+			File file = File.createTempFile("qrauth-rsa-", ".ssh2");
+
+			try
+			{
+				writeBytesToFile(pubKey.getBytes(), file);
+				sshFormat = Exec.toString(SSH_KEYGEN.toString(), "-i", "-m", "PEM", "-f", file.toString());
+			}
+			finally
+			{
+				file.delete();
+			}
+		}
+		else
+		if (pubKey.contains("BEGIN PUBLIC KEY"))
+		{
+			pemFormat=pubKey;
+		}
+		else
+		{
+			throw new UnsupportedEncodingException("unknown public key format, or not an rsa key");
+		}
+	}
+
+	private
+	String sshFormat;
+
+	private
+	File sshFile;
+
+	private
+	String pemFormat;
+
+	private
+	File pemFile;
+
+	public
+	File getSshFile() throws IOException
+	{
+		if (sshFile==null)
+		{
+			sshFile = File.createTempFile("qrauth-rsa-", ".pem");
+			writeBytesToFile(getSshFormat().getBytes(), sshFile);
+		}
+		return sshFile;
+	}
+
+	public
+	String getSshFormat() throws IOException
+	{
+		if (sshFormat==null)
+		{
+			if (!PUBKEY2SSH.canExecute())
+			{
+				throw new UnsupportedOperationException("sorry, unable to support PEM -> SSH public key conversion at this time");
+			}
+
+			sshFormat = Exec.toString(PUBKEY2SSH.toString(), getPemFile().getAbsolutePath(), "pem-no-comment");
+		}
+
+		return sshFormat;
+	}
+
+	private
+	File getPemFile() throws IOException
+	{
+		if (pemFile==null)
+		{
+			pemFile = File.createTempFile("qrauth-rsa-", ".pem");
+			writeBytesToFile(getPemFormat().getBytes(), pemFile);
+		}
+
+		return pemFile;
+	}
+
+	public
+	String getPemFormat() throws IOException
+	{
+		if (pemFormat==null)
+		{
+			if (!SSH_KEYGEN.canExecute())
+			{
+				throw new UnsupportedOperationException("sorry, unable to support SSH -> PEM public key conversion at this time");
+			}
+
+			pemFormat = Exec.toString(SSH_KEYGEN.toString(), "-f", getSshFile().getAbsolutePath(), "-e", "-m", "PKCS8");
+		}
+
+		return pemFormat;
+	}
+
+	/*
+	Base64.decodeBase64(base64Response)
+	 */
+	public
+	boolean signatureIsValid(String data, byte[] signature) throws IOException
+	{
+		final
+		File signatureFile = File.createTempFile("qrauth-rsa-", ".sig");
+
+		try
+		{
+			writeBytesToFile(signature, signatureFile);
+
+			//Uggh... it is very inconvenient to use pkeyutl due to a widespread exit-status bug...
+			if (OPENSSL_BUG_2618_IS_STILL_WIDESPREAD)
+			{
+				final
+				String signedData = Exec.toString("/usr/bin/openssl", "rsautl", "-verify", "-in",
+													 signatureFile.getAbsolutePath(), "-pubin", "-inkey",
+													 getPemFile().getAbsolutePath());
+
+				return signedData.equals(data);
+			}
+			else
+			{
+				//echo -n ${NUT} | openssl pkeyutl -verify -sigfile ${SIGNED} -pubin -inkey ${PEM}
+
+				Exec.withInput(data.getBytes(),
+								  "/usr/bin/openssl", "pkeyutl", "-verify", "-sigfile",
+								  signatureFile.getAbsolutePath(), "-pubin", "-inkey",
+								  getPemFile().getAbsolutePath());
+
+				return true;
+			}
+		}
+		finally
+		{
+			signatureFile.delete();
+		}
+	}
+
+	private
+	void writeBytesToFile(byte[] bytes, File file) throws IOException
+	{
+		final
+		OutputStream out=new FileOutputStream(file);
+
+		try
+		{
+			out.write(bytes);
+		}
+		finally
+		{
+			out.close();
+		}
+	}
+
+	@Override
+	public
+	void close() throws IOException
+	{
+		if (pemFile!=null)
+		{
+			pemFile.delete();
+			pemFile=null;
+		}
+
+		if (sshFile!=null)
+		{
+			sshFile.delete();
+			sshFile=null;
+		}
+	}
+}
--- java/qrauth-server/src/main/java/com/allogy/qrauth/server/pages/internal/auth/DispatchAuth.java
+++ java/qrauth-server/src/main/java/com/allogy/qrauth/server/pages/internal/auth/DispatchAuth.java
@@ -4,6 +4,7 @@ import com.allogy.qrauth.server.entities.Nut;
 import com.allogy.qrauth.server.helpers.Death;
 import com.allogy.qrauth.server.helpers.ErrorResponse;
 import com.allogy.qrauth.server.helpers.Exec;
+import com.allogy.qrauth.server.helpers.RSAHelper;
 import com.allogy.qrauth.server.pages.api.AbstractAPICall;
 import org.apache.commons.codec.binary.Base64;
 import org.hibernate.criterion.Restrictions;
@@ -19,17 +20,6 @@ import java.io.OutputStream;
 public
 class DispatchAuth extends AbstractAPICall
 {
-	private static final
-	File SSH_KEYGEN = new File(System.getProperty("SSH_KEYGEN", "/usr/bin/ssh-keygen"));
-
-	private static final
-	File PUBKEY2SSH = new File(System.getProperty("PUBKEY2SSH", "/usr/bin/qrauth-pubkey2ssh"));
-
-	/**
-	 * @url http://rt.openssl.org/Ticket/Display.html?user=guest&pass=guest&id=2618
-	 */
-	private static final
-	boolean OPENSSL_BUG_2618_IS_STILL_WIDESPREAD = true;
 
 	Object onActivate() throws IOException
 	{
@@ -135,147 +125,19 @@ class DispatchAuth extends AbstractAPICall
 		else
 		{
 			final
-			String pubKey=pubKeyOrUsername;
+			RSAHelper rsaHelper=new RSAHelper(pubKeyOrUsername);
 
-			final
-			String sshRsaFormat;
-
-			final
-			File pemFile;
+			if (rsaHelper.signatureIsValid(nut.stringValue, Base64.decodeBase64(base64Response)))
 			{
-				if (pubKey.contains("ssh-rsa"))
-				{
-					//They provided the public key in SSH-RSA format
-					sshRsaFormat=pubKey;
 
-					if (!SSH_KEYGEN.canExecute())
-					{
-						log.error("cannot execute: {}", SSH_KEYGEN);
-						return new ErrorResponse(500, "sorry, unable to support ssh-rsa keys at this time");
-					}
-
-					final
-					File tempSshRsaFile=File.createTempFile("qrauth-rsa-ssh-",".pub");
-
-					try
-					{
-						writeBytesToFile(sshRsaFormat.getBytes(), tempSshRsaFile);
-
-						final
-						String pemData = Exec.toString(SSH_KEYGEN.toString(), "-f", tempSshRsaFile.getAbsolutePath(), "-e", "-m", "PKCS8");
-
-						log.debug("pemData:\n{}", pemData);
-
-						pemFile=File.createTempFile("qrauth-rsa-ssh-",".pem");
-						writeBytesToFile(pemData.getBytes(), pemFile);
-					}
-					finally
-					{
-						tempSshRsaFile.delete();
-					}
-				}
-				else
-				if (pubKey.contains("BEGIN SSH2 PUBLIC KEY"))
-				{
-					//TODO: support this format too...
-					//---> ssh-keygen -i -f ${KEY}
-					return new ErrorResponse(500, "that pubkey format is not yet supported :-(");
-				}
-				else
-				if (pubKey.contains("BEGIN RSA PUBLIC KEY"))
-				{
-					//TODO: support this format too...
-					//---> ssh-keygen -i -m PEM -f ${RSA}
-					return new ErrorResponse(500, "that pubkey format is not yet supported :-(");
-				}
-				else
-				if (pubKey.contains("BEGIN PUBLIC KEY"))
-				{
-					//They provided the public key in PEM format
-
-					if (!PUBKEY2SSH.canExecute())
-					{
-						log.error("cannot execute: {}", PUBKEY2SSH);
-						return new ErrorResponse(500, "sorry, unable to support PEM public key format at this time");
-					}
-
-					pemFile=File.createTempFile("qrauth-rsa-",".pem");
-					writeBytesToFile(pubKey.getBytes(), pemFile);
-
-					sshRsaFormat = Exec.toString(PUBKEY2SSH.toString(), pemFile.getAbsolutePath(), "pem-no-comment");
-				}
-				else
-				{
-					return invalidParameter("rsa_pubkey");
-				}
+				//TODO: create user & method (with new public key)
+				//return authSuccess();
+				return new ErrorResponse(500, "trying...");
 			}
-
-			try
+			else
 			{
-				log.info("got: {}", sshRsaFormat);
-
-				final
-				File signatureFile=File.createTempFile("qrauth-rsa-",".sig");
-
-				try
-				{
-					writeBytesToFile(Base64.decodeBase64(base64Response), signatureFile);
-
-					//actually do the public key verification...
-
-					//Uggh... it is very inconvenient to use pkeyutl due to a widespread exit-status bug...
-					if (OPENSSL_BUG_2618_IS_STILL_WIDESPREAD)
-					{
-						final
-						String signedNut=Exec.toString("/usr/bin/openssl", "rsautl", "-verify", "-in",
-														  signatureFile.getAbsolutePath(), "-pubin", "-inkey",
-														  pemFile.getAbsolutePath());
-
-						if (!signedNut.equals(nut.stringValue))
-						{
-							log.error("signature mismatch: '{}' != '{}'", signedNut, nut.stringValue);
-							return invalidParameter("rsa_response");
-						}
-					}
-					else
-					{
-						//echo -n ${NUT} | openssl pkeyutl -verify -sigfile ${SIGNED} -pubin -inkey ${PEM}
-
-						Exec.withInput(nut.stringValue.getBytes(),
-										  "/usr/bin/openssl", "pkeyutl", "-verify", "-sigfile",
-										  signatureFile.getAbsolutePath(), "-pubin", "-inkey",
-										  pemFile.getAbsolutePath());
-					}
-				}
-				finally
-				{
-					signatureFile.delete();
-				}
+				return new ErrorResponse(403, "signature problem");
 			}
-			finally
-			{
-				pemFile.delete();
-			}
-
-			//TODO: create user & method (with new public key)
-			//return authSuccess();
-			return new ErrorResponse(500, "trying...");
-		}
-	}
-
-	private
-	void writeBytesToFile(byte[] bytes, File file) throws IOException
-	{
-		final
-		OutputStream out=new FileOutputStream(file);
-
-		try
-		{
-			out.write(bytes);
-		}
-		finally
-		{
-			out.close();
 		}
 	}
 
